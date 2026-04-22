c2ca89f65ded9e1a07cec922ffb08a3069b40238
abidkhokhar628-hue <abidkhokhar628@gmail.com>
2026-04-22 19:34:03 +0500

    para
    para

--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -4,6 +4,7 @@ function Resume() {
   return (
     <div>
       <h1>Resume</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi beatae animi molestiae vel, repellendus accusamus libero iure cupiditate ab, porro odit numquam.</p>
     </div>
   )
 }
