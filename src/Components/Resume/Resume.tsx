5fb9712ee8c0f3c495d1c8a7f7d9335d9dabde44
abidkhokhar628-hue <abidkhokhar628@gmail.com>
2026-04-22 19:39:39 +0500

    ali
    ali

--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -5,6 +5,7 @@ function Resume() {
     <div>
       <h1>Resume</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi beatae animi molestiae vel, repellendus accusamus libero iure cupiditate ab, porro odit numquam.</p>
+      <p>Ali Abid</p>
     </div>
   )
 }
