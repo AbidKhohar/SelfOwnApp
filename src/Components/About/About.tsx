8c0427e021dd42da731e1a0fa5e1d8947aebb564
abidkhokhar628-hue <abidkhokhar628@gmail.com>
2026-04-27 15:16:12 +0500

    UpdareP
    UpdareP

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './About.css';
 import { FaGithub, FaTwitter, FaGoogle, FaLinkedin} from "react-icons/fa";
+import { IoMdTrophy } from "react-icons/io";
+import { GiGraduateCap } from "react-icons/gi";
+import { GoPeople } from "react-icons/go";
+
+
+
 
 const About: React.FC = () => {
   return (
@@ -125,9 +131,131 @@ const About: React.FC = () => {
 
 
 
+
+
+
+<section className="education-section py-5">
+  <div className="container text-center">
+
+    <span className="badge bg-light text-success mb-2">LIFE TIME</span>
+    <h2 className="fw-bold mb-3">Education & Experience</h2>
+    <p className="text-muted mb-5">
+      We craft digital, graphic and dimensional thinking, to create category<br/>
+      leading brand experiences that have meaning.
+    </p>
+
+    {/* ITEM 1 */}
+    <div className="timeline-item red">
+      <div className="row align-items-center">
+
+        <div className="col-md-2 text-center">
+          <div className="icon-box">
+            <i className="bi bi-trophy"><IoMdTrophy/></i>
+          </div>
+        </div>
+
+        <div className="col-md-7 text-start">
+          <p className="fw-bold">GHI Themes - Web & App IT Company</p>
+          <p className="text-muted mb-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem.
+          </p>
+        </div>
+
+        <div className="col-md-3 text-center">
+          <div className="year-circle">2019-22</div>
+        </div>
+
+      </div>
+    </div>
+
+    {/* ITEM 2 */}
+    <div className="timeline-item yellow">
+      <div className="row align-items-center">
+
+        <div className="col-md-3 text-center">
+          <div className="year-circle">2014-18</div>
+        </div>
+
+        <div className="col-md-7 text-start">
+          <p className="fw-bold">ABC Themes - Web Design IT Company</p>
+          <p className="text-muted mb-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem.
+          </p>
+        </div>
+
+        <div className="col-md-2 text-center">
+          <div className="icon-box">
+            <i className="bi bi-people"><GoPeople/></i>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    {/* ITEM 3 */}
+    <div className="timeline-item green">
+      <div className="row align-items-center">
+
+        <div className="col-md-2 text-center">
+          <div className="icon-box">
+            <i className="bi bi-mortarboard"><GiGraduateCap/></i>
+          </div>
+        </div>
+
+        <div className="col-md-7 text-start">
+          <p className="fw-bold">MSc IT Master Degree</p>
+          <p className="text-muted mb-0">
+            Harvard University is an educational institution that offers graduate, professional programs.
+          </p>
+        </div>
+
+        <div className="col-md-3 text-center">
+          <div className="year-circle">2012-13</div>
+        </div>
+
+
+
+
+
+      </div>
+      
+    </div>
+
+
+
+    {/* ITEM 2 */}
+    <div className="timeline-item yellow">
+      <div className="row align-items-center">
+
+        <div className="col-md-3 text-center">
+          <div className="year-circle">2014-18</div>
+        </div>
+
+        <div className="col-md-7 text-start">
+          <p className="fw-bold">High / Higher Secondry School</p>
+          <p className="text-muted mb-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem.
+          </p>
+        </div>
+
+        <div className="col-md-2 text-center">
+          <div className="icon-box">
+            <i className="bi bi-people"><GoPeople/></i>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</section>
+
+
+
+
       {/* ========== FREELANCER CTA SECTION ========== */}
       <section className="freelancer-cta py-5">
-        <div className="container">
+        <div className="container-sm">
           <div className="row align-items-center">
             <div className="col-lg-6">
               <h2 className="cta-title">I Am Available For Freelancer.</h2>
@@ -142,8 +270,6 @@ const About: React.FC = () => {
           </div>
         </div>
       </section>
-
-
 
     </>
   );
--- src/Components/Contacts/Contacts.tsx
+++ src/Components/Contacts/Contacts.tsx
@@ -1,9 +1,90 @@
 import React from 'react'
+import {  FaPhone, FaEnvelope  } from "react-icons/fa";
 
 function Contacts() {
   return (
     <div>
-      <h1>Contacts</h1> 
+      {/* About Hero Section */}
+      <section className="about-hero">
+        <div className="container-fluid h-100">
+          <div className="row justify-content-center align-items-center h-100">
+            <div className="col-12 text-center">
+              <h5 className="page-title fw-bold">Contact Me</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+      {/* ========== CONTACT SECTION ========== */}
+      <section className="contact py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <h2 className="contact-title">Get in touch !</h2>
+              <p className="contact-description">
+                Always available for freelancing if the right project comes along, Feel free to contact me.
+              </p>
+
+              <form className="contact-form mt-4">
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <input type="text" placeholder="Your name" className="form-control contact-input" />
+                  </div>
+                  <div className="col-md-6 mb-3">
+                    <input type="email" placeholder="Your email" className="form-control contact-input" />
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <input type="text" placeholder="your subject" className="form-control contact-input" />
+                  </div>
+                  <div className="col-md-6 mb-3">
+                    <input type="text" placeholder="+00 1234 5678 90" className="form-control contact-input" />
+                  </div>
+                </div>
+
+                <div className="mb-3">
+                  <textarea placeholder="Enter your message..." className="form-control contact-input" rows={5}></textarea>
+                </div>
+              </form>
+              <button className="btn btn-primary mt-3">Send Message</button>
+
+            </div>
+            <div className="col-lg-6">
+              <div className="contact-info">
+                <div className="contact-info-item">
+                  <div className="contact-info-icon">
+                    <i className="fas fa-phone"><FaPhone/></i>
+                  </div>
+                  <div>
+                    <h6>+1234 567 89</h6>
+                    <p>9:00am to 7:00pm</p>
+                  </div>
+                </div>
+
+                <div className="contact-info-item">
+                  <div className="contact-info-icon">
+                    <i className="fas fa-envelope"><FaEnvelope/></i>
+                  </div>
+                  <div>
+                    <h6>example@example.com</h6>
+                    <p>Monday to Saturday</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
     </div>
   )
 }
